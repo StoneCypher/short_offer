44adc6301b7b3723b8dd239ae9c05f19b987c6af
John Haugeland <stonecypher@gmail.com>
2021-08-09 01:10:52 -0700

    Connection string set fixes #16, tracks size fixes #12

---
+++ build/typescript/testset.spec.js
@@ -0,0 +1,5 @@
+import { compile, decompile } from './index';
+import * as fc from 'fast-check';
+test('Round trip of random strings is always byte-accurate', () => {
+    fc.assert(fc.property(fc.string(), (anyString) => decompile(compile(anyString)) === anyString));
+});
---
+++ src/build_js/record_stats_for_version.js
@@ -0,0 +1,70 @@
+
+const fs = require('fs');
+
+const package = JSON.parse(`${ fs.readFileSync('./package.json') }`);
+
+const logfile = './src/maintained_artifacts/stats_by_version.json',
+      log     = JSON.parse(`${ fs.readFileSync(logfile) }`);
+
+const full_set = require('../ts/example_beacons.js').full_set,
+      styles   = Object.keys(full_set);
+
+const so = require('../../build/rollup/index.cjs.js');
+
+
+
+
+
+function generate_test_log() {
+
+  const ret = {};
+
+  let orig   = 0,
+      final  = 0,
+      failed = 0,
+      time   = 0
+
+  styles.map( s => {
+
+    const original_length = full_set[s].length,
+          start_time      = new Date().getTime(),
+          final_length    = so.compile(full_set[s]).length,
+          end_time        = new Date().getTime(),
+          failed_claims   = 0,
+          ltime           = end_time - start_time;
+
+    ret[s] = {
+
+      original_length,
+      final_length,
+      failed_claims,
+      time: ltime
+
+    };
+
+    orig   += original_length;
+    final  += final_length;
+    failed += failed_claims;
+    time   += ltime;
+
+  } )
+
+  ret['total'] = {
+    variations      : styles.length,
+    original_length : orig,
+    final_length    : final,
+    failed_claims   : failed,
+    time
+  }
+
+  return ret;
+
+}
+
+
+
+
+
+log[ package.version ] = generate_test_log();
+
+fs.writeFileSync( logfile, JSON.stringify(log, undefined, 2) );
